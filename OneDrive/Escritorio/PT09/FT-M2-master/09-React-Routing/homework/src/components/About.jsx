04c1d9e756c54b5386802d742f22a2a8dbf79679
adrianavinicki <adriana.vinicki@gmail.com>
2022-10-06 19:32:41 -0300

    6.10 commit

---
+++ OneDrive/Escritorio/PT09/FT-M2-master/09-React-Routing/homework/src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function About(){
+    return(
+    <div>Hola soy About y voy a contar sobre la ciudad</div>
+    )
+}
--- OneDrive/Escritorio/PT09/FT-M2-master/09-React-Routing/homework/src/components/Cards.jsx
+++ OneDrive/Escritorio/PT09/FT-M2-master/09-React-Routing/homework/src/components/Cards.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Cards.css';
-
 import Card from './Card.jsx';
 
 export default function Cards({cities, onClose}) {
   return (
     <div className='cards'>
       {cities.map(c => <Card
+          id={c.id}
           key={c.id}
           max={c.max}
           min={c.min}
