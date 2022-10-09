bed18f17be85e4470753df15d7676e5f3b02f5f9
adrianavinicki <adriana.vinicki@gmail.com>
2022-10-09 13:04:00 -0300

    09.10 commit

--- OneDrive/Escritorio/PT09/FT-M2-master/11-Redux/hw-js/src/reducer/index.js
+++ OneDrive/Escritorio/PT09/FT-M2-master/11-Redux/hw-js/src/reducer/index.js
@@ -8,8 +8,22 @@ const initialState = {
 // Recibe el estado de nuestro store, junto con una action creada por nuestro action creator. 
 // ¿Qué tiene que hacer el reducer con el contador de cada caso?
 
+// [...state, newstate] estoy concatenando
+//{...state, contador=5} copio state y reemplazo el valor de la propiedad contador
+
 function contador(state = initialState, action) {
-  
+  switch(action.type){
+   case INCREMENTO :
+    //podria haber algun tipo de logica, filter map o lo que querramos, y luego se lo asociamos a una propiedad de nuestro estado
+   return {...state, contador: state.contador + 1};
+
+   case DECREMENTO: 
+   return {...state, contador: state.contador - 1};
+
+   default:
+   return state
+
+  }
 }
 
 module.exports = contador;
